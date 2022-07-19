513ee646ea54b1edc44005020452f88b5713237b
Federico Rotondo <fdrcrotondomza@gmail.com>
2022-07-19 11:43:29 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SinTestComponent } from './components/sin-test/sin-test.component';
+import { StudentsComponent } from './components/students/students.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinTestComponent,
+    FooterComponent,
+    StudentsComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
